93d07852e54d3f3b2b80970995fa461d3db79005
jari <mohitjari09@gmail.com>
2026-02-17 03:56:01 +0530

    firebase-host

--- student-dashboard/src/environments/environment.prod.ts
+++ student-dashboard/src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://result-server-po2j.onrender.com/api'
+  apiUrl: 'https://result-server-po2j.onrender.com/api',
+  firebase : {
+    apiKey: "AIzaSyCpJ0Aqf-94YBRBGKfpSxAAlO_vxdTclok",
+    authDomain: "academic-progress-dashboard.firebaseapp.com",
+    projectId: "academic-progress-dashboard",
+    storageBucket: "academic-progress-dashboard.firebasestorage.app",
+    messagingSenderId: "139665969452",
+    appId: "1:139665969452:web:5166c3fad52c2e41a55c89"
+  }
 };
